713f7ac3632d396901b19540fdc90621cf092be0
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-05-18 19:13:23 +0200

    save localBusiness

--- app/controllers/entrepreneurs/edit/step-1.js
+++ app/controllers/entrepreneurs/edit/step-1.js
@@ -3,8 +3,19 @@ import { action } from '@ember/object';
 
 export default class EntrepreneursEditStep1Controller extends Controller {
 
+  async saveAll(localBusiness){
+    for(const hourSpec of (await localBusiness.openingHoursSpecifications).toArray()){
+      await hourSpec.save();
+    }
+    for(const category of (await localBusiness.categories).toArray()){
+      await category.save();
+    }
+    await (await localBusiness.location).save();
+  }
+
   @action
-  nextStep(localBusiness){
+  async nextStep(localBusiness){
+    await this.saveAll(localBusiness);
     this.transitionToRoute('entrepreneurs.edit.step-2', localBusiness);
   }
 
