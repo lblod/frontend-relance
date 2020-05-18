a9b498ffa30d6691cf2924b2228b931b32c2ca18
Erika Pauwels <erika.pauwels@gmail.com>
2020-05-18 14:33:13 +0200

    Correcty call super in native ES6 class

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -3,6 +3,6 @@ import JSONAPISerializer from '@ember-data/serializer/json-api';
 export default class ApplicationSerializer extends JSONAPISerializer {
   serializeAttribute(snapshot, json, key, attributes) {
     if (key !== 'uri')
-      this._super(snapshot, json, key, attributes);
+      super.serializeAttribute(snapshot, json, key, attributes);
   }
 }
