7088bc6ff234af1cda98fb96e0ca16f60c18d93f
Erika Pauwels <erika.pauwels@gmail.com>
2020-05-18 14:28:36 +0200

    Add uri property to ember-data models

--- app/models/category.js
+++ app/models/category.js
@@ -1,5 +1,6 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class CategoryModel extends Model {
+  @attr uri;
   @attr label;
 }
--- app/models/opening-hours-specification.js
+++ app/models/opening-hours-specification.js
@@ -1,11 +1,12 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class OpeningHoursSpecificationModel extends Model {
+  @attr uri;
   @attr('date') validFrom;
   @attr('date') validThrough;
   @attr  opens;
   @attr  closes;
-  
+
   @belongsTo('local-business') localBusiness;
   @belongsTo('day-of-week') dayOfWeek;
 }
