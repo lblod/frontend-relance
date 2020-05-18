cab35dae0acf499ffd05c26800bb38bc757b0dcc
Erika Pauwels <erika.pauwels@gmail.com>
2020-05-18 16:03:16 +0200

    Add model hook for entrepreneurs new and edit routes

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,6 @@
 import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
-
   model() {
     const business = this.store.createRecord('local-business', {name: 'felix'});
     return business.save();
