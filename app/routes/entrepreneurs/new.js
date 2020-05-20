fb73709ce5dfd6d730ff100d0d322d893e5e4ab5
Aad Versteden <madnificent@gmail.com>
2020-05-20 07:17:16 +0200

    Remove static example hours specification and insert dynamic one.
    Calculation happens in the model, looks safe for use.

--- app/routes/entrepreneurs/new.js
+++ app/routes/entrepreneurs/new.js
@@ -2,10 +2,34 @@ import Route from '@ember/routing/route';
 
 export default class EntrepreneursNewRoute extends Route {
   async model() {
+    // make sure we have a location
     const location = this.store.createRecord('location');
-    const business = this.store.createRecord('local-business', { location });
+
+    // make sure we know the opening hours
+    const firstDay = (await this.store.query('day-of-week', {
+      sort: 'position',
+      page: { size: 1 }
+    })).firstObject;
+
+    const openingHours = [this.store.createRecord('opening-hours-specification', {
+      opens: "09:30:00",
+      closes: "18:00:00",
+      dayOfWeek: firstDay
+    })];
+
+    // create the business
+    const business = this.store.createRecord('local-business', { location, openingHours });
+
+    // backlink the business
     location.localBusiness = business;
+    openingHours.forEach( (o) => o.localBusiness = business );
+
+    // create leaf instances
+    for( const h of openingHours )
+      await h.save();
     await location.save();
+
+    // create the business itself
     return business.save();
   }
 
