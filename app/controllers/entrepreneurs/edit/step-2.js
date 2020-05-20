4445c111c9c0dd03f5e156477b3231ee4c575e75
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-05-20 13:24:22 +0200

    toggle code block

--- app/controllers/entrepreneurs/edit/step-2.js
+++ app/controllers/entrepreneurs/edit/step-2.js
@@ -1,10 +1,18 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class EntrepreneursEditStep2Controller extends Controller {
   @service router;
+  @tracked displayCodeBlock = true;
 
   get urlForSubjectPage(){
     return window.location.origin + this.router.urlFor('entrepreneurs.subject-pages.show', this.model.id);
+  }
+
+  @action
+  toggleCodeBlock(){
+    this.displayCodeBlock = !this.displayCodeBlock;
   }
 }
