06185306d1a90689dbc5db29aaa9e192bb87671e
Aad Versteden <madnificent@gmail.com>
2020-05-20 16:23:12 +0200

    Add scroll to top on page change
    When pages change, we want the page to scroll to the top.  This
    change implements just that.

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,10 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+    historySupportMiddleware: true,
+    routerScroll: {
+      scrollWhenAfterRender: true
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
