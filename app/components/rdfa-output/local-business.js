f49f0d99335439ce4e090f034bc74863a20def3e
Aad Versteden <madnificent@gmail.com>
2020-05-28 22:28:08 +0200

    Better support of Fastboot loaded content
    The main goal for Fastboot is to render the subjectpages.  Most work
    has happened in that.  The current solution fetches data up front so
    we're can do away with postponed loading.  There is room for
    improvement but it's a sane start.

--- app/components/rdfa-output/local-business.js
+++ app/components/rdfa-output/local-business.js
@@ -1,3 +1,4 @@
+import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 import { task } from 'ember-concurrency-decorators';
 import { tracked } from '@glimmer/tracking';
@@ -9,9 +10,14 @@ export default class RdfaOutputLocalBusinessComponent extends Component {
   @tracked openingHours = []
   @tracked rdfaSnippet
 
+  @service fastboot;
+
   constructor() {
     super(...arguments);
-    this.loadData.perform();
+    if( ! this.fastboot.isFastboot )
+      this.loadData.perform();
+    else
+      this.syncLoadData();
   }
 
   @task
@@ -21,24 +27,34 @@ export default class RdfaOutputLocalBusinessComponent extends Component {
     this.categories = (yield this.localBusiness.categories).toArray();
     this.naceBelCodes = (yield this.localBusiness.naceBelCodes).toArray();
     this.openingHours = (yield this.localBusiness.openingHoursSpecifications).toArray();
-    this.generateRdfaSnippet.perform();
+    for (const oh of this.openingHours )
+      yield this.openingHours.dayOfWeek;
+    this.generateRdfaSnippet();
   }
 
-  @task
-  *generateRdfaSnippet() {
+  syncLoadData() {
+    this.localBusiness = this.args.localBusiness;
+    this.location = this.localBusiness.location;
+    this.categories = this.localBusiness.categories.toArray();
+    this.naceBelCodes = this.localBusiness.naceBelCodes.toArray();
+    this.openingHours = this.localBusiness.openingHoursSpecifications.toArray();
+    this.generateRdfaSnippet();
+  }
+
+  generateRdfaSnippet() {
     const name = this.localBusiness.name ? `<div property="schema:name">${this.localBusiness.name}</div>` : '';
     const description = this.localBusiness.description ? `<div property="schema:description">${this.localBusiness.description}</div>` : '';
     const website = this.localBusiness.url ? `<a property="schema:url" href="${this.localBusiness.url}">${this.localBusiness.url}</a>` : '';
     const email = this.localBusiness.email ? `<a property="schema:email" href="${this.localBusiness.email}">${this.localBusiness.email.slice('mailto:'.length)}</a>` : '';
-    const phone = this.localBusiness.telephone ? `<a property="schema:telephone" content="${this.localBusiness.telephone}">${this.localBusiness.telephone.slice('tel:'.length)}</a>` : '';
+    const phone = this.localBusiness.telephone ? `<a property="schema:telephone" content="${this.localBusiness.telephone}">${this.localBusiness.get('telephone').slice('tel:'.length)}</a>` : '';
 
     let location = '';
     if (this.location) {
-      const street = this.location.streetAddress ? `<div property="schema:streetAddress">${this.location.streetAddress}</div>` : '';
-      const postalCode = this.location.postalCode ? `<div property="schema:postalCode">${this.location.postalCode}</div>` : '';
-      const city = this.location.city ? `<div property="schema:addressLocality">${this.location.city}</div>` : '';
+      const street = this.location.get('streetAddress') ? `<div property="schema:streetAddress">${this.location.get('streetAddress')}</div>` : '';
+      const postalCode = this.location.get('postalCode') ? `<div property="schema:postalCode">${this.location.get('postalCode')}</div>` : '';
+      const city = this.location.get('city') ? `<div property="schema:addressLocality">${this.location.get('city')}</div>` : '';
       location = `
-        <div property="schema:address" resource="${this.location.uri}" typeof="schema:PostalAddress">
+        <div property="schema:address" resource="${this.location.get('uri')}" typeof="schema:PostalAddress">
           ${street}
           ${postalCode}
           ${city}
@@ -47,10 +63,10 @@ export default class RdfaOutputLocalBusinessComponent extends Component {
 
     let openingHours = '';
     for (let openingHour of this.openingHours) {
-      const dayOfWeek = yield openingHour.dayOfWeek;
-      const day = dayOfWeek ? `<span property="schema:dayOfWeek" href="${dayOfWeek.uri}">${dayOfWeek.name}</span>` : '';
-      const opens = openingHour.opens ? `<span property="schema:opens" content="${openingHour.opens}" datatype="xsd:time">${openingHour.opens}</span>` : '';
-      const closes = openingHour.closes ? `<span property="schema:closes" content="${openingHour.closes}" datatype="xsd:time">${openingHour.closes}</span>` : '';
+      const dayOfWeek = openingHour.dayOfWeek;
+      const day = dayOfWeek ? `<span property="schema:dayOfWeek" href="${dayOfWeek.get('uri')}">${dayOfWeek.get('name')}</span>` : '';
+      const opens = openingHour.opens ? `<span property="schema:opens" content="${openingHour.get('opens')}" datatype="xsd:time">${openingHour.get('opens')}</span>` : '';
+      const closes = openingHour.closes ? `<span property="schema:closes" content="${openingHour.get('closes')}" datatype="xsd:time">${openingHour.get('closes')}</span>` : '';
       const validFromStr = openingHour.validFrom ? openingHour.validFrom.toISOString().substr(0, 10) : null;
       const validFrom = validFromStr ? `<span property="schema:validFrom" content="${validFromStr}" datatype="xsd:date">${validFromStr}</span>` : '';
       const validThroughStr = openingHour.validThrough ? openingHour.validThrough.toISOString().substr(0, 10) : null;
@@ -81,7 +97,7 @@ export default class RdfaOutputLocalBusinessComponent extends Component {
     }
 
     this.rdfaSnippet = `
-        <div resource="${this.args.localBusiness.uri}"
+        <div resource="${this.localBusiness.uri}"
              typeof="schema:LocalBusiness ${categories} ${nacebelUris}"
             prefix="schema: http://schema.org/ nacebel: http://data.gift/vocabularies/nace-bel/ skos: http://www.w3.org/2004/02/skos/core#">
           ${naceBelCodes}
--- app/routes/entrepreneurs/subject-pages/show.js
+++ app/routes/entrepreneurs/subject-pages/show.js
@@ -1,7 +1,18 @@
+import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 
 export default class EntrepreneursSubjectPagesShowRoute extends Route {
-  model(params){
-    return this.store.find('local-business', params.id);
+  @service fastboot;
+
+  async model(params){
+    if( this.fastboot.isFastboot ) {
+      const businesses = await this.store.query( 'local-business', {
+        "filter[:id:]": params.id,
+        include: "location,categories,nace-bel-codes,opening-hours-specifications,opening-hours-specifications,opening-hours-specifications.day-of-week"
+      });
+      return businesses.firstObject;
+    } else {
+      return  await this.store.find('local-business', params.id);
+    }
   }
 }
