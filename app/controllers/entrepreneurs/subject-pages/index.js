ee1f70016e736c06daa4c63af67d2b75712a69d2
Erika Pauwels <erika.pauwels@gmail.com>
2020-06-03 17:17:33 +0200

    Add pagination on entrepreneurs overview page

--- app/controllers/entrepreneurs/subject-pages/index.js
+++ app/controllers/entrepreneurs/subject-pages/index.js
@@ -1,4 +1,6 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { computed } from '@ember/object';
 
 export default class EntrepreneursSubjectPagesIndexController extends Controller {
   sort = 'name';
@@ -12,4 +14,40 @@ export default class EntrepreneursSubjectPagesIndexController extends Controller
       return null;
     }
   }
+
+  @computed('page', 'size')
+  get rangeStart() {
+    return this.page * this.size + 1;
+  }
+
+  @computed('rangeStart', 'size', 'total')
+  get rangeEnd() {
+    const end = this.rangeStart + this.size - 1;
+    return end > this.total ? this.total : end;
+  }
+
+  @computed('page')
+  get isFirstPage() {
+    return this.page == 0;
+  }
+
+  @computed('rangeEnd', 'total')
+  get isLastPage() {
+    return this.rangeEnd == this.total;
+  }
+
+  get total() {
+    return this.model.meta.count;
+  }
+
+  @action
+  previousPage() {
+    this.set('page', this.page - 1);
+  }
+
+  @action
+  nextPage() {
+    this.set('page', this.page + 1);
+  }
+
 }
