d69ed8b650e4984c0081bca46e71ae59f40fafa9
Erika Pauwels <erika.pauwels@gmail.com>
2020-06-03 20:01:59 +0200

    Add navigation to overview page per city in header

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,9 +4,22 @@ import Controller from '@ember/controller';
 
 export default class ApplicationController extends Controller {
   @service appState
+  @service router
 
   @action
   setAppDidRender(){
     this.appState.didRender = true;
+  }
+
+  @action
+  goToSubjectsOverviewPage() {
+    let postalCode = this.postalCode || '2800';
+    this.postalCode = null;
+    this.router.transitionTo('entrepreneurs.subject-pages', {
+      queryParams: {
+        page: 0,
+        postalCode: postalCode
+      }
+    });
   }
 }
