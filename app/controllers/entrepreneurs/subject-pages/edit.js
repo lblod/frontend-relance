e2b5abc5e23f9f3e916c06f580a3b97ef5f114ae
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-06-04 10:33:36 +0200

    refactor the edit subject page

--- app/controllers/entrepreneurs/subject-pages/edit.js
+++ app/controllers/entrepreneurs/subject-pages/edit.js
@@ -1,11 +1,48 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency-decorators';
+import { timeout } from 'ember-concurrency';
 
 export default class EntrepreneursSubjectPagesEditController extends Controller {
+  @service router;
   overviewRoute = "entrepreneurs.subject-pages.index";
-  
+  @tracked displayCodeBlock = true;
+
+  get urlForSubjectPage(){
+    if (window.location)
+      return window.location.origin + this.router.urlFor('entrepreneurs.subject-pages.show', this.model.id);
+    else
+      return this.router.urlFor('entrepreneurs.subject-pages.show', this.model.id);
+  }
+
+  @task
+  *saveAll(localBusiness){
+    yield timeout(300);
+    for(const hourSpec of (yield localBusiness.openingHoursSpecifications).toArray()){
+      yield hourSpec.save();
+    }
+    for(const category of (yield localBusiness.categories).toArray()){
+      yield category.save();
+    }
+    localBusiness.modified = new Date();
+    yield (yield localBusiness.location).save();
+    yield localBusiness.save();
+  }
+
   @action
-  backToOverview(){
+  toggleCodeBlock(){
+    this.displayCodeBlock = !this.displayCodeBlock;
+  }
+
+  @action
+  back(){
     this.transitionToRoute(this.overviewRoute);
+  }
+
+  @action
+  save(localBusiness){
+    this.saveAll.perform(localBusiness);
   }
 }
